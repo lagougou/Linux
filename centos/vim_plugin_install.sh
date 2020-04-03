0aa6b15cccb4da48060c4b8edd98778990675ad4
lagougou <deraiven@126.com>
2020-04-03 16:26:19 +0800

    add vim config file

---
+++ centos/vim_plugin_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
